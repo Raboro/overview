350f02a9f4b8e207f4add283b6125c848a384b9d
Raboro <marius.woerfelreal@gmail.com>
2026-01-10 21:31:40 +0100

    feat(voucher): Add basic put

--- server/voucher/src/main/java/io/github/raboro/overview/voucher/VoucherController.java
+++ server/voucher/src/main/java/io/github/raboro/overview/voucher/VoucherController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,5 +34,19 @@ public class VoucherController {
     @DeleteMapping("{voucherId}")
     public ResponseEntity<Void> deleteById(@PathVariable("voucherId") long id) {
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{voucherId}")
+    public ResponseEntity<VoucherDTO> updateById(@PathVariable("voucherId") long id,
+                                                 @RequestBody VoucherCreationDTO newVoucher) {
+        return ResponseEntity.ok(
+                new VoucherDTO(
+                    id,
+                    newVoucher.getName(),
+                    newVoucher.getValue(),
+                    newVoucher.getExpiratonDate(),
+                    newVoucher.isRedeemed()
+            )
+        );
     }
 }
