1257699c2d13ee7286590ad05afd5e463a3e424f
Raboro <marius.woerfelreal@gmail.com>
2026-01-12 23:20:43 +0100

    feat(testing): :sparkles: Add basic IT

---
+++ server/overview-server/src/test/java/io/github/raboro/overview/server/VoucherIT.java
@@ -0,0 +1,31 @@
+package io.github.raboro.overview.server;
+
+import static io.restassured.RestAssured.given;
+
+import io.github.raboro.overview.voucher.VoucherCreationDTO;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+class VoucherIT extends OverviewIT {
+
+    @Test
+    void test() {
+        long id = given()
+                .contentType(ContentType.JSON)
+                .body(new VoucherCreationDTO("name", 10, LocalDate.now(), false))
+                .when()
+                .post("/api/overview-server/v1/voucher-app/voucher")
+                .then()
+                .statusCode(202)
+                .extract().body().as(Long.class);
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/overview-server/v1/voucher-app/voucher/" + id)
+                .then()
+                .statusCode(200);
+    }
+}
