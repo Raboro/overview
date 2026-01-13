6b2a83d5ab310cbce0a818a9f7906504436a427c
Marius Wörfel <marius.woerfelreal@gmail.com>
2026-01-13 01:30:38 +0100

    Feature/test container setup (#2)
    * feat(testing): :sparkles: Add testing for ITs
    
    * feat(testing): :sparkles: Add IT config
    
    * feat(testing): :sparkles: Add IT abstraction with postgresql
    
    * feat(testing): :sparkles: Add basic IT
    
    * feat(testing): :sparkles: Enhance test and logging
    
    * feat(testing): :sparkles: Make IT public

---
+++ server/overview-server/src/test/java/io/github/raboro/overview/server/VoucherIT.java
@@ -0,0 +1,27 @@
+package io.github.raboro.overview.server;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.github.raboro.overview.voucher.VoucherCreationDTO;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+public class VoucherIT extends OverviewIT {
+
+    @Test
+    public void creationOfVoucherShouldWork() {
+        long id = given()
+                .contentType(ContentType.JSON)
+                .body(new VoucherCreationDTO("name", 10, LocalDate.now(), false))
+                .when()
+                .post("/api/overview-server/v1/voucher-app/voucher")
+                .then()
+                .statusCode(202)
+                .extract().body().as(Long.class);
+
+        assertTrue(id > 0);
+    }
+}
