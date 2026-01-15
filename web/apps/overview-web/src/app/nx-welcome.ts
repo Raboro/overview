33ee60c9c56a2d9ac1ebe58d6198e00e72a731da
Raboro <marius.woerfelreal@gmail.com>
2026-01-15 23:54:40 +0100

    feat(web): :sparkles: Use service to call backend

--- web/apps/overview-web/src/app/nx-welcome.ts
+++ web/apps/overview-web/src/app/nx-welcome.ts
@@ -1,11 +1,24 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-nx-welcome',
   imports: [CommonModule],
-  template: `<h1>Welcome overview-web 👋</h1>`,
+  template: `<h1>Welcome overview-web 👋</h1>
+    <button (click)="getAll()">Get All</button
+    ><button (click)="add()">Add new</button>`,
   styles: [],
   encapsulation: ViewEncapsulation.None,
 })
-export class NxWelcome {}
+export class NxWelcome {
+  appService = inject(AppService);
+
+  getAll() {
+    this.appService.getAll();
+  }
+
+  add() {
+    this.appService.add();
+  }
+}
