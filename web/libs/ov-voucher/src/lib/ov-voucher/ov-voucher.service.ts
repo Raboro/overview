1820e714f7d03c6640f55a9b067c18d416e0cca2
Raboro <marius.woerfelreal@gmail.com>
2026-01-18 20:52:49 +0100

    feat(web): :sparkles: Include results of voucher call in frontend and add possibility to use different view types as well as rendering resutls on page load

--- web/libs/ov-voucher/src/lib/ov-voucher/ov-voucher.service.ts
+++ web/libs/ov-voucher/src/lib/ov-voucher/ov-voucher.service.ts
@@ -1,18 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class OvVoucherService {
   private http = inject(HttpClient);
 
-  getAll() {
-    this.http
-      .get<
-        Voucher[]
-      >('http://localhost:8080/api/overview-server/v1/voucher-app/voucher')
-      .subscribe((voucher) => {
-        console.log(voucher);
-      });
+  getAll(): Observable<Voucher[]> {
+    return this.http.get<Voucher[]>(
+      'http://localhost:8080/api/overview-server/v1/voucher-app/voucher',
+    );
   }
 
   add() {
@@ -32,11 +29,11 @@ export class OvVoucherService {
   }
 }
 
-interface Voucher extends VoucherCreation {
+export interface Voucher extends VoucherCreation {
   id: number;
 }
 
-interface VoucherCreation {
+export interface VoucherCreation {
   name: string;
   value: number;
   expirationDate: Date;
--- web/libs/ov-voucher/src/lib/ov-voucher/ov-voucher.ts
+++ web/libs/ov-voucher/src/lib/ov-voucher/ov-voucher.ts
@@ -1,17 +1,36 @@
-import { OvVoucherService } from './ov-voucher.service';
-import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { OvVoucherService, Voucher } from './ov-voucher.service';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { startWith, Subject, switchMap } from 'rxjs';
+
 
 @Component({
   selector: 'ov-voucher',
-  imports: [],
+  imports: [MatIconModule, MatButtonToggleModule],
   templateUrl: './ov-voucher.html',
   styleUrl: './ov-voucher.scss',
 })
-export class OvVoucher {
-  ovVoucherService = inject(OvVoucherService);
+export class OvVoucher implements OnInit {
+  protected ovVoucherService = inject(OvVoucherService);
 
-  getAll() {
+  protected refresh$ = new Subject<void>()
+  protected viewType = signal<'list' | 'grid'>('list');
+  protected vouchers = toSignal(
+    this.refresh$.pipe(
+      startWith(void 0),
+      switchMap(() => this.ovVoucherService.getAll())
+    ),
+    { initialValue: [] }
+  )
+
+  ngOnInit(): void {
     this.ovVoucherService.getAll();
+  }
+
+  refresh() {
+    this.refresh$.next();
   }
 
   add() {
