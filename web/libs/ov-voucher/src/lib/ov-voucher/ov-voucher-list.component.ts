d6e02e181e30d41d7fe5f1bd2da40bb61488baf6
Raboro <marius.woerfelreal@gmail.com>
2026-01-21 01:31:39 +0100

    feature(voucher): :sparkles: Add basic list view

--- web/libs/ov-voucher/src/lib/ov-voucher/ov-voucher-list.component.ts
+++ web/libs/ov-voucher/src/lib/ov-voucher/ov-voucher-list.component.ts
@@ -1,16 +1,47 @@
 import { Component, input } from "@angular/core";
 import { Voucher } from "./ov-voucher.service";
+import { MatCardModule } from "@angular/material/card";
+import { MatChipsModule } from "@angular/material/chips";
 
 @Component({
   selector: 'ov-voucher-list',
   standalone: true,
+  imports: [
+    MatCardModule,
+    MatChipsModule,
+  ],
   template: `
-    <ul>
+    <div class="cards-container">
       @for (voucher of vouchers(); track voucher.id) {
-        <li>{{ voucher.name }}</li>
+        <mat-card class="voucher-card" appearance="outlined">
+          <mat-card-header>
+            <mat-card-title>{{ voucher.name }}</mat-card-title>
+          </mat-card-header>
+          <mat-card-content>
+            <mat-chip-set>
+              <mat-chip>{{ voucher.expirationDate }}</mat-chip>
+              <mat-chip>{{ voucher.value }}€</mat-chip>
+            </mat-chip-set>
+          </mat-card-content>
+        </mat-card>
       }
-    </ul>
-  `
+    </div>
+  `,
+  styles: [
+    `
+    .cards-container {
+      display: flex;
+      flex-direction: column;
+      gap: 2vh;
+      height: 70vh;
+      overflow-y: auto;
+    }
+
+    .voucher-card {
+      max-height: 15vh;
+    }
+    `
+  ]
 })
 export class OvVoucherListComponent {
   vouchers = input.required<Voucher[]>();
