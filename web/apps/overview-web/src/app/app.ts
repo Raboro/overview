5b68046e9efd29a5bd2dbd3fd5d19f295a502871
Raboro <marius.woerfelreal@gmail.com>
2026-02-26 00:24:05 +0100

    refactor(web): :recycle: Enhance styling

--- web/apps/overview-web/src/app/app.ts
+++ web/apps/overview-web/src/app/app.ts
@@ -1,15 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { OvVoucher } from '@ov/ov-voucher';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  imports: [OvVoucher, RouterModule],
+  imports: [OvVoucher, RouterModule, MatIcon],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
-  apps = ['Voucher'];
+  apps: AppWrapper[] = [{ name: 'Voucher', iconName: 'local_activity' }];
   selectedApp = signal<string | null>(null);
 
   openApp(app: string) {
@@ -20,3 +21,8 @@ export class App {
     this.selectedApp.set(null);
   }
 }
+
+type AppWrapper = {
+  name: string;
+  iconName: string;
+};
