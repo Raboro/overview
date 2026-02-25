8d457871cd687138e5290a3a17c95e5da554df7a
Raboro <marius.woerfelreal@gmail.com>
2026-02-26 00:29:51 +0100

    fix(web): :bug: Correct command to execute e2e tests

--- web/apps/overview-web-e2e/playwright.config.ts
+++ web/apps/overview-web-e2e/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'bun nx run overview-web:serve',
+    command: 'bun run serve',
     url: 'http://localhost:4200',
     reuseExistingServer: true,
     cwd: workspaceRoot,
